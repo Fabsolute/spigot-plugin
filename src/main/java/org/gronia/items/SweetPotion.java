10cd2dcbcc6713eb65e920371e639c1b3e176c2f
Fabsolutely <ahmetturk93@gmail.com>
2021-12-11 00:24:47 +0300

    M  src/main/java/org/gronia/items/SweetPotion.java

--- src/main/java/org/gronia/items/SweetPotion.java
+++ src/main/java/org/gronia/items/SweetPotion.java
@@ -1,12 +1,14 @@
 package org.gronia.items;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
-import org.bukkit.Material;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.*;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.gronia.plugin.Gronia;
 import org.gronia.plugin.ItemRegistry;
 import org.gronia.plugin.fatigue.FatiguePlugin;
@@ -14,7 +16,9 @@ import org.gronia.plugin.fatigue.FatigueUtil;
 import org.gronia.plugin.uei.*;
 import org.gronia.utils.pair.Pair;
 
+import javax.naming.Name;
 import java.util.List;
+import java.util.logging.Level;
 
 public abstract class SweetPotion extends CustomItem implements TierableItem, CraftableItem<CustomShapedRecipe>, EventListenerItem {
     private final int tier;
@@ -49,9 +53,13 @@ public abstract class SweetPotion extends CustomItem implements TierableItem, Cr
         super.onMetaCreate(meta);
         PotionMeta p = (PotionMeta) meta;
         p.setColor(getColor());
-        meta.setLore(List.of(
-                ChatColor.BLUE + "+" + getFatigueRefresh()
-        ));
+        meta.lore(this.getLore(meta));
+    }
+
+    protected List<Component> getLore(ItemMeta meta) {
+        return List.of(
+                Component.text("+" + getFatigueRefresh(), NamedTextColor.BLUE)
+        );
     }
 
     protected boolean onConsume(PlayerItemConsumeEvent event) {
@@ -178,8 +186,19 @@ public abstract class SweetPotion extends CustomItem implements TierableItem, Cr
     }
 
     public static class Epic extends SweetPotion {
+        private final static int maxLevel = 4;
+        private NamespacedKey percentageLevelKey;
+
         public Epic() {
             super(5, ItemNames.EPIC_ENCHANTED_SWEET_POTION, "Epic Enchanted Sweet Potion");
+            Bukkit.getLogger().log(Level.WARNING, "ya aq");
+        }
+
+        @Override
+        public void onEnable() {
+            super.onEnable();
+            this.percentageLevelKey = Gronia.getInstance().getKey("epic_esp.percentage");
+            Bukkit.getLogger().log(Level.WARNING, "ff" + this.percentageLevelKey);
         }
 
         @Override
@@ -188,8 +207,43 @@ public abstract class SweetPotion extends CustomItem implements TierableItem, Cr
                 return false;
             }
 
-            Gronia.getInstance().getSubPlugin(FatiguePlugin.class).getUtil().changeSteroid(event.getPlayer(), 7200);
+            var item = event.getItem();
+            var meta = item.getItemMeta();
+            var level = this.getLevel(meta);
+            level++;
+
+            Gronia.getInstance().getSubPlugin(FatiguePlugin.class).getUtil().changeSteroid(event.getPlayer(), 1800);
+
+            if (level == maxLevel) {
+                return true;
+            }
+
+            setLevel(meta, level);
+            meta.lore(this.getLore(meta));
+            item.setItemMeta(meta);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Gronia.getInstance(), () -> {
+                event.getPlayer().getInventory().addItem(item);
+                event.getPlayer().getInventory().removeItem(new ItemStack(Material.GLASS_BOTTLE));
+            }, 1);
             return true;
+        }
+
+        private int getLevel(ItemMeta meta) {
+            if (this.percentageLevelKey == null) {
+                Bukkit.getLogger().log(Level.WARNING, "Wut");
+                return 0;
+            }
+
+            var level = meta.getPersistentDataContainer().get(this.percentageLevelKey, PersistentDataType.INTEGER);
+            if (level == null) {
+                return 0;
+            }
+
+            return level;
+        }
+
+        private void setLevel(ItemMeta meta, int level) {
+            meta.getPersistentDataContainer().set(this.percentageLevelKey, PersistentDataType.INTEGER, level);
         }
 
         @Override
@@ -207,6 +261,14 @@ public abstract class SweetPotion extends CustomItem implements TierableItem, Cr
         @Override
         int getFatigueRefresh() {
             return FatigueUtil.MAX_RESTNESS;
+        }
+
+        protected List<Component> getLore(ItemMeta meta) {
+            var level = this.getLevel(meta);
+            return List.of(
+                    Component.text("+" + getFatigueRefresh(), NamedTextColor.BLUE),
+                    Component.text((maxLevel - level) + "/" + maxLevel, NamedTextColor.GREEN)
+            );
         }
     }
 }
